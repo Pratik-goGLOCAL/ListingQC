33ad570d92523925438678cba0a3f7f45bf65c82
Pratik Ghongade <pratik.g@goglocal.com>
2023-02-20 13:39:58 +0530

    Name of brand added in the QC results

--- send_email.py
+++ send_email.py
@@ -2,20 +2,26 @@ from send_mail import SendMail
 import streamlit as st
 import os
 
+if st.session_state['asin_brand']=='ASIN':
+    resfor = 'ASIN'
+    names = st.session_state['ASIN']
+else:
+    resfor = st.session_state['Brand_name']
+    names = st.session_state['Brand_name']
 def send_email(r_email,filename):
     # Create SendMail object
     new_mail = SendMail(
         # List (or string if single recipient) of the email addresses of the recipients
         [r_email], 
         # Subject of the email
-        'Listing QC Results',
+        'Listing QC Results for '+resfor,
         # Body of the email
         '''Hello,
 
-        The Listing QC check is sucessfully completed!!!
+        The Listing QC check for {} is sucessfully completed!!!
         The results are attached with this mail.
         
-        Thanks''', 
+        Thanks'''.format(names), 
         # Email address of the sender
         # Leave this paramter out if using environment variable 'EMAIL_ADDRESS'
         'pratik.g@goglocal.com' 
